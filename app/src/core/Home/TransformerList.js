d4353d9f4cc4829f28b2a6f85a2df90fc29d9831
Stanley <Stanleyliu15@gmail.com>
2019-06-16 22:12:40 -0700

    make title of transformer list a prop

--- app/src/core/Home/TransformerList.js
+++ app/src/core/Home/TransformerList.js
@@ -4,21 +4,25 @@ import PropTypes from 'prop-types';
 import TransformerListItem from './TransformerListItem';
 import computeTransformerRating from '../../util/computeTransformerRating';
 
-const TransformerList = ({ transformers }) => (
-  <ul>
-    {transformers.map(transformer => (
-      <TransformerListItem
-        key={transformer.id}
-        id={transformer.id}
-        name={transformer.name}
-        rating={computeTransformerRating(transformer)}
-      />
-    ))}
-  </ul>
+const TransformerList = ({ title, transformers }) => (
+  <div>
+    <h3>{title}</h3>
+    <ul>
+      {transformers.map(transformer => (
+        <TransformerListItem
+          key={transformer.id}
+          id={transformer.id}
+          name={transformer.name}
+          rating={computeTransformerRating(transformer)}
+        />
+      ))}
+    </ul>
+  </div>
 );
 
 TransformerList.propTypes = {
-  transformers: PropTypes.arrayOf(PropTypes.object)
+  title: PropTypes.string.isRequired,
+  transformers: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default TransformerList;
