56160bf0d0f8adfda34f22fc3c79661b538d28e0
Stanley <Stanleyliu15@gmail.com>
2019-06-17 13:11:54 -0700

    add font-awesome for the icons

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
